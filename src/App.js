37769d544750eb61b0fe4c22528f968fbe736354
MeryAmun <chebesiawah@gmail.com>
2021-11-24 16:32:27 +0100

    about page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css'
 
+import About from './components/About'
 import Home from './components/Home'
 import Navbar from './components/Navbar'
 import React from 'react'
@@ -9,6 +10,7 @@ function App() {
     <>
       <Navbar />
       <Home />
+      <About />
     </>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div>
+      <section id='about'>
+        <div className='container my-5 py-5'>
+          <div className='row'>
+            <div className='col-md-6'>
+              <img src='/assets/about.jpg' alt='about' className='w-75 mt-5' />
+            </div>
+            <div className='col-md-6'>
+              <h3 className='fs-5 mb-0'>About Us</h3>
+              <h1 className='display-6 mb-2'>
+                Who<b>we</b> Are
+              </h1>
+              <hr className='w-50' />
+              <p className='lead mb-4'>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Delectus, omnis? Mollitia quaerat voluptatum nostrum odit
+                reiciendis? Aliquid unde corporis ipsa distinctio! Voluptatum
+                officia excepturi voluptatem id pariatur harum nam nisi ducimus
+                reiciendis? Neque odio, ab repellendus omnis sint autem rerum
+                magnam repellat, debitis laudantium accusantium ratione
+                temporibus. At, iusto tempora?
+              </p>
+              <button className='btn btn-primary rounded-pill px-4 py-2'>
+                Get Started
+              </button>
+              <button className='btn btn-outline-primary rounded-pill px-4 py-2 ms-2'>
+                Contact Us
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  )
+}
